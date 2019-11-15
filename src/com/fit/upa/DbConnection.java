52711d01fb545d320df6c616e2e91b00ff7d2521
mariannociar <majo.nociar@gmail.com>
2019-11-15 17:52:01 +0100

    Initialization of javafx and rework DBconn to separate class

--- src/com/fit/upa/DbConnection.java
+++ src/com/fit/upa/DbConnection.java
@@ -1,41 +1,49 @@
 package com.fit.upa;
-
 import java.sql.*;
 
 public class DbConnection {
+    private static DbConnection dbConn = null;
+    private boolean connected = false;
+    private Connection conn;
 
-    public static void main(String[] args) {
-        //Open a connection
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:oracle:thin:@//gort.fit.vutbr.cz:1521/orclpdb","xnocia00", "eWHhifOx")) {
+    private DbConnection(){}
 
-            if (conn != null) {
-                System.out.println("Connected to the database!");
-            } else {
-                System.out.println("Failed to make connection!");
-            }
-            //Execute a query
-            Statement stmt=conn.createStatement();
-
-            ResultSet rs=stmt.executeQuery("select * from Students");
-            //Extract data from result set
-            while(rs.next()){
-                int id = rs.getInt("id");
-                String login = rs.getString("login");
-                String fullname = rs.getString("fullName");
-                System.out.print("ID: " + id + "   ");
-                System.out.print("LOGIN: " + login + "   ");
-                System.out.println("FULL NAME: " + fullname);
-            }
-            //Clean-up
-            rs.close();
-            stmt.close();
-        //Handle errors
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static DbConnection getInstance(){
+        if(dbConn == null) {
+            dbConn = new DbConnection();
         }
+        return dbConn;
+    }
 
+    public void connect() {
+        if(!connected) {
+            try {
+                conn = DriverManager.getConnection("jdbc:oracle:thin:@//gort.fit.vutbr.cz:1521/orclpdb", "xnocia00", "eWHhifOx");
+                connected = true;
+            } catch (SQLException e) {
+                connected = false;
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void disconnect() {
+        if(connected){
+            try {
+                conn.close();
+                connected = false;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.out.println("Already disconnected");
+        }
+    }
+    public Connection getConn(){
+        return conn;
+    }
+    public boolean isConnected(){
+        return connected;
     }
 }
---
+++ src/com/fit/upa/Main.java
@@ -0,0 +1,41 @@
+package com.fit.upa;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        DbConnection dbConn = DbConnection.getInstance();
+        dbConn.connect();
+        if(dbConn.isConnected()){
+            System.out.println("we are successfuly connected");
+        }
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        DbConnection dbConn = DbConnection.getInstance();
+            System.out.println(dbConn.isConnected());
+        //creating a Group object
+        Group group = new Group();
+
+        //Creating a Scene by passing the group object, height and width
+        Scene scene = new Scene(group ,600, 300);
+
+        //setting color to the scene
+        scene.setFill(Color.BROWN);
+
+        //Setting the title to Stage.
+        primaryStage.setTitle("Sample Application");
+
+        //Adding the scene to Stage
+        primaryStage.setScene(scene);
+
+        //Displaying the contents of the stage
+        primaryStage.show();
+    }
+}
