51f366d3bae0bfede34918669f9b72124ea1b89e
mariannociar <majo.nociar@gmail.com>
2019-11-16 16:07:34 +0100

    Drawing polygons from database

--- src/com/fit/upa/Main.java
+++ src/com/fit/upa/Main.java
@@ -1,8 +1,12 @@
 package com.fit.upa;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -31,13 +35,19 @@ public class Main extends Application {
         //creating a Group object
         Group group = new Group();
 
+        Scale scale = new Scale(); //origin to bottom left corner
+        scale.setY(-1);
+        scale.pivotYProperty().bind(Bindings.createDoubleBinding(() -> group.getBoundsInLocal().getMaxY()+group.getBoundsInLocal().getHeight()/2,group.boundsInLocalProperty()));
+        group.getTransforms().add(scale);
+
+
         Shapes shapes = new Shapes(arrayList, group);
 
         //Creating a Scene by passing the group object, height and width
         Scene scene = new Scene(group ,600, 300);
 
         //setting color to the scene
-        //scene.setFill(Color.BROWN);
+        scene.setFill(Paint.valueOf("f5f5f5"));
 
         //Setting the title to Stage.
         primaryStage.setTitle("Sample Application");
--- src/com/fit/upa/Shapes.java
+++ src/com/fit/upa/Shapes.java
@@ -5,6 +5,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Shapes {
     Group root;
@@ -18,6 +19,40 @@ public class Shapes {
                 System.out.println(r.name);
                 root.getChildren().add(r);
             }
+            else if(elem.type == 1){
+                shapes.add(new Poly(elem.ordinates, elem.name, root));
+                Poly p = (Poly) shapes.get(shapes.size() - 1);
+                System.out.println(p.name);
+                root.getChildren().add(p);
+            }
+        }
+    }
+
+    class Poly extends Polyline{
+        String name;
+        Double[] ordinates;
+        Group poly = new Group();
+        public Poly(double[] ordinates, String name, Group g){
+            this.name = name;
+            //this.ordinates = ordinates;
+            int pointsCount = ordinates.length/2-1;
+            this.ordinates = new Double[pointsCount*2];
+            Circle[] polyCorners = new Circle[pointsCount];
+            for(int i=0; i<pointsCount; i++){
+                this.ordinates[2*i] = ordinates[2*i];
+                this.ordinates[2*i+1] = ordinates[2*i+1];
+                polyCorners[i] = new Circle(ordinates[2*i], ordinates[2*i+1], 0);
+            }
+            for(Circle c: polyCorners){
+                c.setFill(Color.TOMATO.deriveColor(1,1,1,0.5));
+                c.setStroke(Color.TOMATO);
+                poly.getChildren().add(c);
+            }
+            System.out.println(Arrays.toString(this.ordinates));
+            super.getPoints().addAll(this.ordinates);
+            g.getChildren().add(poly);
+            setFill(Color.DARKGRAY.deriveColor(1,1,1,1));
+            setStroke(Color.GRAY);
         }
     }
 
@@ -28,8 +63,8 @@ public class Shapes {
         public Rec(double[] ordinates, String name, Group g){
             this.name = name;
             this.ordinates = ordinates;
-            Circle[] recCorners= {new Circle(ordinates[0], ordinates[1], 5),
-                    new Circle(ordinates[2], ordinates[3], 5)};
+            Circle[] recCorners= {new Circle(ordinates[0], ordinates[1], 0),
+                    new Circle(ordinates[2], ordinates[3], 0)};
             for(Circle c: recCorners){
                 c.setFill(Color.TOMATO.deriveColor(1,1,1,0.5));
                 c.setStroke(Color.TOMATO);
