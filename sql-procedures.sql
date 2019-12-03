621500b969f6dbf11ce24bafc31120a3f1a21ce1
krajc <xkrajc17@stud.fit.vutbr.cz>
2019-12-03 22:24:40 +0100

    created procedures

--- sql-procedures.sql
+++ sql-procedures.sql
@@ -1,29 +1,37 @@
 CREATE INDEX map_geometry_sidx ON map(geometry) INDEXTYPE IS MDSYS.SPATIAL_INDEX;
 
--- Najde vsetky pozemky do ktorych zasahuje electric area
-SELECT estate.name, estate.type building
-FROM map estate, map earea
-WHERE  (estate.type = 'estate') AND (earea.COtype = 'electric-area') AND
-	(SDO_RELATE(estate.geometry, earea.geometry, 'mask=ANYINTERACT') = 'TRUE')
-ORDER BY estate.name ASC;
+Create procedure EstatesElectricArea
+AS
+  SELECT estate.name, estate.type building
+  FROM map estate, map earea
+  WHERE  (estate.type = 'estate') AND (earea.COtype = 'electric-area') AND
+  	(SDO_RELATE(estate.geometry, earea.geometry, 'mask=ANYINTERACT') = 'TRUE')
+  ORDER BY estate.name ASC;
+GO;
 
--- Pozemky s gas-mast
-SELECT estate.name, estate.type building
-FROM map estate, map earea
-WHERE  (estate.type = 'estate') AND (earea.type = 'gas-mast') AND
-	(SDO_RELATE(estate.geometry, earea.geometry, 'mask=ANYINTERACT') = 'TRUE')
-ORDER BY estate.name ASC;
+Create procedure EstatesGasMast
+AS
+  SELECT estate.name, estate.type building
+  FROM map estate, map earea
+  WHERE  (estate.type = 'estate') AND (earea.type = 'gas-mast') AND
+  	(SDO_RELATE(estate.geometry, earea.geometry, 'mask=ANYINTERACT') = 'TRUE')
+  ORDER BY estate.name ASC;
+GO;
 
--- do ktorych zasahuje gas area
-SELECT estate.name, estate.type building
-FROM map estate, map earea
-WHERE  (estate.type = 'estate') AND (earea.type = 'gas-area') AND
-	(SDO_RELATE(estate.geometry, earea.geometry, 'mask=ANYINTERACT') = 'TRUE')
-ORDER BY estate.name ASC;
+Create procedure EstatesGasArea
+AS
+  SELECT estate.name, estate.type building
+  FROM map estate, map earea
+  WHERE  (estate.type = 'estate') AND (earea.type = 'gas-area') AND
+  	(SDO_RELATE(estate.geometry, earea.geometry, 'mask=ANYINTERACT') = 'TRUE')
+  ORDER BY estate.name ASC;
+GO;
 
--- Budovy ktore su v gas zone
-SELECT build.name, build.type building
-FROM map build, map earea
-WHERE  (build.type = 'build') AND (earea.type = 'gas-area') AND
-	(SDO_RELATE(build.geometry, earea.geometry, 'mask=ANYINTERACT') = 'TRUE')
-ORDER BY build.name ASC;
+create procedure BuildsInGasArea
+AS
+  SELECT build.name, build.type building
+  FROM map build, map earea
+  WHERE  (build.type = 'build') AND (earea.type = 'gas-area') AND
+  	(SDO_RELATE(build.geometry, earea.geometry, 'mask=ANYINTERACT') = 'TRUE')
+  ORDER BY build.name ASC;
+GO;
