c867c0e89de715bdd3db7dff01f6e020aed41565
krajc <xkrajc17@stud.fit.vutbr.cz>
2019-12-10 23:30:40 +0100

    Logout update

--- src/com/fit/upa/MainMenu.java
+++ src/com/fit/upa/MainMenu.java
@@ -1,17 +1,10 @@
 package com.fit.upa;
 
-import com.fit.upa.shapes.PolyInfo;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
-import javax.xml.ws.handler.LogicalHandler;
 import java.io.IOException;
 
 public class MainMenu {
